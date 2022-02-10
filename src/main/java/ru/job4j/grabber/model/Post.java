6882e30955384543fa9b735b1cf44d58620abcf8
MaratKhatipov <mmmarat86@gmail.com>
2022-02-10 15:51:17 +0500

    2.2. Модель данных - Post. [#285211]

---
+++ src/main/java/ru/job4j/grabber/model/Post.java
@@ -0,0 +1,68 @@
+package ru.job4j.grabber.model;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Post {
+
+    private int id;
+    private String title;
+    private String description;
+    private LocalDateTime created;
+
+    public Post(int id, String title, String description, LocalDateTime created) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.created = created;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Post post = (Post) o;
+        return id == post.id && title.equals(post.title) && created.equals(post.created);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, created);
+    }
+}
