f43cb22afc03bb58ce0fe0aeed9482e2bbc2d44a
MaratKhatipov <mmmarat86@gmail.com>
2022-02-10 14:34:13 +0500

    2.1. Преобразование даты [#289476]

--- src/main/java/ru/job4j/grabber/utils/SqlRuDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/SqlRuDateTimeParser.java
@@ -2,13 +2,46 @@ package ru.job4j.grabber.utils;
 
 import java.time.LocalDateTime;
 import java.util.Map;
+import java.time.format.DateTimeFormatter;
 
 public class SqlRuDateTimeParser implements DateTimeParser {
+    private static final Map<String, String> MONTHS = Map.ofEntries(
+            Map.entry("вчера", LocalDateTime.now().minusDays(1)
+                    .format(
+                    DateTimeFormatter.ofPattern("dd M yy"))),
+            Map.entry("сегодня", LocalDateTime.now().format(
+                    DateTimeFormatter.ofPattern("dd M yy"))),
+            Map.entry("янв", "1"),
+            Map.entry("фев", "2"),
+            Map.entry("мар", "3"),
+            Map.entry("апр", "4"),
+            Map.entry("май", "5"),
+            Map.entry("июн", "6"),
+            Map.entry("июл", "7"),
+            Map.entry("авг", "8"),
+            Map.entry("сен", "9"),
+            Map.entry("окт", "10"),
+            Map.entry("ноя", "11"),
+            Map.entry("дек", "12"));
 
-    private static final Map<String, String> MONTHS = Map.ofEntries();
+    private static final DateTimeFormatter FORMAT_PARSER1 =
+            DateTimeFormatter.ofPattern("dd M yy HH:mm");
 
     @Override
     public LocalDateTime parse(String parse) {
-        return null;
+        String parseDT = "";
+        String[] dateSplit = parse.split("[, ]+");
+        if (dateSplit.length == 4) {
+            parseDT = String.join(
+                    " ",
+                    dateSplit[0], MONTHS.get(dateSplit[1]), dateSplit[2], dateSplit[3]);
+        }
+        if (dateSplit.length == 2) {
+            String check = MONTHS.get(dateSplit[0]);
+            parseDT = String.join(
+                    " ",
+                    MONTHS.get(dateSplit[0]), dateSplit[1]);
+        }
+        return LocalDateTime.parse(parseDT, FORMAT_PARSER1);
     }
 }
---
+++ src/test/java/ru/job4j/grabber/utils/SqlRuDateTimeParserTest.java
@@ -0,0 +1,37 @@
+package ru.job4j.grabber.utils;
+
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+public class SqlRuDateTimeParserTest {
+
+    @Test
+    public void whenParseJanThenJan() {
+        SqlRuDateTimeParser test = new SqlRuDateTimeParser();
+        String dateTest = "20 янв 22, 09:40";
+        LocalDateTime actual = test.parse(dateTest);
+        LocalDateTime expected = LocalDateTime.of(2022, 1, 20, 9, 40);
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void whenParseYesterdayThenYesterday() {
+        SqlRuDateTimeParser test = new SqlRuDateTimeParser();
+        String test1 = "вчера, 05:08";
+        LocalDateTime actual = test.parse(test1);
+        LocalDateTime expected = LocalDateTime.of(2022, 2, 9, 5, 8);
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void whenParseToday() {
+        SqlRuDateTimeParser test = new SqlRuDateTimeParser();
+        String test1 = "сегодня, 05:08";
+        LocalDateTime actual = test.parse(test1);
+        LocalDateTime expected = LocalDateTime.of(2022, 2, 10, 5, 8);
+        assertThat(actual, is(expected));
+    }
+}
