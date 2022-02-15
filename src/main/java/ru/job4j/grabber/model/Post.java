5a465625b2bf133425201e5a42b1d04f2fe74105
MaratKhatipov <mmmarat86@gmail.com>
2022-02-15 10:22:11 +0500

    2.2. Модель данных - Post. [#285211 #262897] добавил конструктор с ID

--- src/main/java/ru/job4j/grabber/model/Post.java
+++ src/main/java/ru/job4j/grabber/model/Post.java
@@ -4,19 +4,15 @@ import java.time.LocalDateTime;
 import java.util.Objects;
 
 public class Post {
-
     private int id;
     private String title;
-    private  String link;
+    private String link;
     private String description;
     private LocalDateTime created;
 
     public Post(int id, String title, String link, String description, LocalDateTime created) {
+        this(title, link, description, created);
         this.id = id;
-        this.title = title;
-        this.link = link;
-        this.description = description;
-        this.created = created;
     }
 
     public Post(String title, String link, String description, LocalDateTime created) {
