bb523d4c4a8fcae4a2ed2b2fd93adf09d1fc7d4e
MaratKhatipov <mmmarat86@gmail.com>
2022-02-15 10:27:26 +0500

    2.3 Загрузка деталей поста

--- src/main/java/ru/job4j/grabber/html/SqlRuParse.java
+++ src/main/java/ru/job4j/grabber/html/SqlRuParse.java
@@ -4,46 +4,71 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.Parse;
 import ru.job4j.grabber.model.Post;
+import ru.job4j.grabber.utils.DateTimeParser;
 import ru.job4j.grabber.utils.SqlRuDateTimeParser;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SqlRuParse {
-    public static void main(String[] args) throws IOException {
+
+    private final DateTimeParser dateTimeParser;
+
+    public SqlRuParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
+    }
+/*
+    @Override
+    public List<Post> list(String link) throws IOException {
+        List<Post> postList = new ArrayList<>();
         for (int i = 1; i < 6; i++) {
-            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + i).get();
+            Document doc = Jsoup.connect(link + i).get();
             Elements row = doc.select(".postslisttopic");
             for (Element td : row) {
                 Element href = td.child(0);
                 Element parent = td.parent();
-                System.out.println(href.text());
-                System.out.println(href.attr("href"));
-                System.out.println(parent.child(5).text() + System.lineSeparator());
+                postList.add(detail(href.attr("href")));
             }
         }
-        String link = "https://www.sql.ru/forum/"
-                + "1325330/lidy-be-fe-senior-cistemnye-analitiki-qa-i-devops-moskva-do-200t";
-        System.out.println(detail(link));
-
-        String link2 = "https://www.sql.ru/forum/1342124/v-poiskah-java-developer";
-        System.out.println(detail(link2));
+        return postList;
     }
 
-    public static Post detail(String link) throws IOException {
-        Post post = new Post();
+ */
+
+    public Post detail(String link) throws IOException {
         Document document = Jsoup.connect(link).get();
         Elements row = document.select("td.msgBody");
         Elements titleData = document.select(".messageHeader");
-        post.setLink(link);
-        post.setTitle(titleData.parents().get(0).text());
-        post.setDescription(row.get(1).text());
-        post.setId(Integer.parseInt(
-                document.select("td.msgFooter").get(0).child(0).text()));
+
         String[] arr = document.select(".msgFooter").get(0).text().split("\\[+");
         SqlRuDateTimeParser parser = new SqlRuDateTimeParser();
-        post.setCreated(parser.parse(arr[0]));
-        return post;
+        LocalDateTime created = parser.parse(arr[0]);
+
+        String title = titleData.parents().get(0).text();
+        String description = row.get(1).text();
+        return new Post(title, link, description, created);
     }
+/*
+    public static void main(String[] args) throws IOException {
+        String link = "https://www.sql.ru/forum/job-offers/";
+
+        SqlRuDateTimeParser sqlRuDateTimeParser = new SqlRuDateTimeParser();
+        SqlRuParse sqlRuParse = new SqlRuParse(sqlRuDateTimeParser);
+
+        List<Post> postList = sqlRuParse.list(link);
+
+        System.out.println(postList.size());
+
+        Post test = postList.get(3);
+        System.out.println(test.getDescription());
+        System.out.println(test.getTitle());
+        System.out.println(test.getLink());
+
+    }
+
+ */
 }
