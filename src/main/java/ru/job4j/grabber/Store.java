91941f65a74d52c26f7f8d240ad092059210d027
MaratKhatipov <mmmarat86@gmail.com>
2022-02-15 15:15:38 +0500

    3. Архитектура проекта - Аргегатор Java Вакансий [#260359]

---
+++ src/main/java/ru/job4j/grabber/Store.java
@@ -0,0 +1,20 @@
+package ru.job4j.grabber;
+
+import ru.job4j.grabber.model.Post;
+
+import java.util.List;
+
+/**
+ * Метод save() - сохраняет объявление в базе.
+ *
+ * Метод getAll() - позволяет извлечь объявления из базы.
+ *
+ * Метод findById(int id) - позволяет извлечь объявление из базы по id.
+ */
+public interface Store {
+    void save(Post post);
+
+    List<Post> getAll();
+
+    Post findById();
+}
