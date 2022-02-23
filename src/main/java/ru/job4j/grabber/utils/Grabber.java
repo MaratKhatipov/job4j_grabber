9df419365d9f830d2610739a9abe4bb68b592df7
MaratKhatipov <mmmarat86@gmail.com>
2022-02-23 16:03:06 +0500

    7. Web. [#289478] v1.01

--- src/main/java/ru/job4j/grabber/utils/Grabber.java
+++ src/main/java/ru/job4j/grabber/utils/Grabber.java
@@ -9,6 +9,9 @@ import ru.job4j.grabber.html.SqlRuParse;
 import ru.job4j.grabber.model.Post;
 
 import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.Charset;
 import java.util.List;
 import java.util.Properties;
 
@@ -53,10 +56,32 @@ public class Grabber implements Grab {
         scheduler.scheduleJob(job, trigger);
     }
 
+    public void web(Store store) {
+        new Thread(() -> {
+            try (ServerSocket server = new ServerSocket(Integer.parseInt(
+                    cfg.getProperty("port")))) {
+                while (!server.isClosed()) {
+                    Socket socket = server.accept();
+                    try (OutputStream out = socket.getOutputStream()) {
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        for (Post post : store.getAll()) {
+                            out.write(post.toString().getBytes(Charset.forName("Windows-1251")));
+                            out.write(System.lineSeparator().getBytes());
+                        }
+                    } catch (IOException io) {
+                        io.printStackTrace();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
     public static class GrabJob implements Job {
 
         @Override
-        public void execute(JobExecutionContext context) throws JobExecutionException {
+        public void execute(JobExecutionContext context) {
             String link = "https://www.sql.ru/forum/job-offers/";
             JobDataMap map = context.getJobDetail().getJobDataMap();
             Store store = (Store) map.get("store");
@@ -66,6 +91,7 @@ public class Grabber implements Grab {
                 store.save(post);
             }
         }
+
     }
 
     public static void main(String[] args) throws IOException, SchedulerException {
@@ -74,5 +100,6 @@ public class Grabber implements Grab {
         Scheduler scheduler = grab.scheduler();
         Store store = grab.store();
         grab.init(new SqlRuParse(new SqlRuDateTimeParser()), store, scheduler);
+        grab.web(store);
     }
 }
